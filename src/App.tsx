1c0bcbb602c1258f36d2151083f754f29cc2d2a1
John <140966972+johnobriendev@users.noreply.github.com>
2025-03-17 09:13:43 -0400

    fix: center view within app

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ function App() {
   const viewMode = useAppSelector(state => state.ui.viewMode);
   
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100 ">
       <Header />
       
       <main>
-        <div className="max-w-full mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-full mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-center align-center">
           {viewMode === 'list' ? <ListView /> : <KanbanView />}
         </div>
       </main>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -45,7 +45,7 @@ const Header: React.FC = () => {
   
   return (
     <header className="bg-white shadow">
-      <div className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
         <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row md:items-center md:justify-between">
          
           <div className="flex items-center">
